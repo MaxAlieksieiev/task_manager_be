5770a507f444376b1e3e3b7874a24f122ca7091a
MaxAlieksieiev <corp.workMA@gmail.com>
2023-02-24 17:36:17 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+require('dotenv').config({
+  path: `${__dirname}/dev.env`,
+});
+
+const db = require('./models/index.js');
+
+const corsOptions = {
+  origin: 'http://localhost:4200',
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+}));
+
+db.sequelize.sync()
+    .then(() => {
+      console.log('Synced db.');
+    })
+    .catch((err) => {
+      console.log('Failed to sync db: ' + err.message);
+    });
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Server run on 8080');
+});
